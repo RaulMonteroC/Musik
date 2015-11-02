2d8334b5d5f7fcdcca64a35f192fff41c4d994af
RaulMonteroC <raulmonteroc01@gmail.com>
2015-11-02 11:18:59 -0400

    Created Core project and basic data access plumming

---
+++ Musik.Core/Data/MusikContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musik.Core.Data
+{
+    public class MusikContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+
+        public MusikContext() : base("MusikDb") { }
+    }
+}
