e293f6b27342676bdfe2b5caadb821eb21e77a1a
RaulMonteroC <raulmonteroc01@gmail.com>
2015-11-25 11:51:57 -0400

    Created SongRepository class to wrap the logic of calling the web api directly

---
+++ Musik.Core.Data/Bussiness/ApiConnector.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musik.Core.Portable.Bussiness
+{
+    internal class ApiConnector
+    {
+        public T Get<T>(string url)
+        {
+            T returnedData;
+            var client = GetHttpClient();
+            var response = client.GetAsync(url).Result;            
+
+            if (response.IsSuccessStatusCode)
+            {
+                var yourcustomobjects = response.Content.ReadAsStringAsync().Result;
+                returnedData = JsonConvert.DeserializeObject<T>(yourcustomobjects);
+            }
+            else
+            {
+                var message = response.StatusCode.ToString() + " : " + response.ReasonPhrase;
+                throw new Exception(message);
+            }
+
+            return returnedData;
+        }
+
+        private HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
---
+++ Musik.Core.Data/Data/SongRepository.cs
@@ -0,0 +1,29 @@
+﻿using Musik.Core.Portable.Bussiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musik.Core.Data.Data
+{
+    public class SongRepository
+    {
+        private ApiConnector connecor;
+        private string baseUrl;
+
+        public SongRepository()
+        {
+            connecor = new ApiConnector();
+            baseUrl = "httP://localhost:8085/";
+        }
+
+        public IEnumerable<Song> GetAll()
+        {
+            var url = baseUrl + "api/song";
+            var songs = connecor.Get<IEnumerable<Song>>(url);
+
+            return songs;
+        }
+    }
+}
